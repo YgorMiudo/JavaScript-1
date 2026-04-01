a97f4d543a48df1450c4418bb592757dcc68deb1
YgorMiudo <araujo.ygor@escola.pr.gov.br>
2026-04-01 11:29:44 +0000

    trabalho finalizado

---
+++ listalivros.js
@@ -0,0 +1,27 @@
+const precosLivros = [
+    {
+        titulo: "Atos", 
+        preco: 35
+    },
+    {
+        titulo: "Corinthians",
+        preco: 45
+    },
+    {
+        titulo: "Genesis",
+        preco: 50
+    },
+    {
+        titulo: "Reis",
+        preco: 20
+    },
+    {
+        titulo: "Salmos",
+        preco: 65
+    },
+    {
+        titulo: "O Hobbit",
+        preco: 10
+    }
+];
+module.exports = precosLivros;
